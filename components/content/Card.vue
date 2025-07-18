8472d5c678d24e4b312b8dc1e2e19183d19c49df
tvband <30714642+tvband@users.noreply.github.com>
2025-07-18 16:25:45 +0900

    cardコンポーネント追加

---
+++ components/content/Card.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="c-card">
+		<div class="c-card-title">
+			title : <slot name="title" mdc-unwrap="p" />
+		</div>
+
+		<div class="c-card-img">
+			img : <slot name="img" mdc-unwrap="p" />
+		</div>
+
+		<div class="c-card-description">
+			description : <slot name="description" mdc-unwrap="p" />
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.c-card {
+	border: 1px solid #ccc;
+	padding: 40px;
+}
+</style>
