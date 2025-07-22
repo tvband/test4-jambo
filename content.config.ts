d8cc61264baf26d392b63a49854786b0ad66d8f2
tvband <30714642+tvband@users.noreply.github.com>
2025-07-22 11:30:15 +0900

    スキーマのzodデバッグ

--- content.config.ts
+++ content.config.ts
@@ -6,6 +6,7 @@ export default defineContentConfig({
 			type: 'page',
 			source: '1.type1/*.md',
 			// メタデータの追加
+			/*
 			schema: z.object({
 				draft: z.boolean().default(false),
 				category: z.enum(['Alps', 'Himalaya', 'Pyrenees']).optional(),
@@ -26,6 +27,19 @@ export default defineContentConfig({
 						alt: z.string(),
 					}),
 				})),
+			}),
+			*/
+			schema: z.object({
+				draft: z.boolean().default(false),
+				category: z.enum(['Alps', 'Himalaya', 'Pyrenees']).optional(),
+				date: z.date(),
+				image: z.object({
+					src: z.string().editor({ input: 'media' }),
+					alt: z.string(),
+				}),
+				slug: z.string().editor({ hidden: true }),
+				icon: z.string().optional().editor({ input: 'icon' }),
+				
 			}),
 		}),
 	}
