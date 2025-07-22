21ddbbdfe4f27aaa7ad92b5dbf2476420b4f8124
tvband <30714642+tvband@users.noreply.github.com>
2025-07-22 11:23:12 +0900

    type1のスキーマ属性のパターン色々追加

--- content.config.ts
+++ content.config.ts
@@ -7,12 +7,26 @@ export default defineContentConfig({
 			source: '1.type1/*.md',
 			// メタデータの追加
 			schema: z.object({
-				tags: z.array(z.string()),
-				image: z.string(),// stringでメディアライブラリ参照可能
+				draft: z.boolean().default(false),
+				category: z.enum(['Alps', 'Himalaya', 'Pyrenees']).optional(),
 				date: z.date(),
-				draft: z.boolean(),
-				number: z.number(),
-			})
+				image: z.object({
+					src: z.string().editor({ input: 'media' }),
+					alt: z.string(),
+				}),
+				slug: z.string().editor({ hidden: true }),
+				icon: z.string().optional().editor({ input: 'icon' }),
+				authors: z.array(z.object({
+					slug: z.string(),
+					username: z.string(),
+					name: z.string(),
+					to: z.string(),
+					avatar: z.object({
+						src: z.string(),
+						alt: z.string(),
+					}),
+				})),
+			}),
 		}),
 	}
 })
