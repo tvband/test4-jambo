19315650fb93e92b2e0df399bb84cf29dd98b8b5
tvband <30714642+tvband@users.noreply.github.com>
2025-07-22 11:45:03 +0900

    スキーマ更新

--- content.config.ts
+++ content.config.ts
@@ -6,6 +6,7 @@ export default defineContentConfig({
 			type: 'page',
 			source: '1.type1/*.md',
 			// メタデータの追加
+			/*
 			schema: z.object({
 				tags: z.array(z.string()),
 				image: z.string(),// stringでメディアライブラリ参照可能
@@ -13,29 +14,28 @@ export default defineContentConfig({
 				draft: z.boolean(),
 				number: z.number(),
 			})
-			/*
-			schema: z.object({
-        draft: z.boolean().default(false),
-        category: z.enum(['Alps', 'Himalaya', 'Pyrenees']).optional(),
-        date: z.date(),
-        image: z.object({
-          src: z.string().editor({ input: 'media' }),
-          alt: z.string(),
-        }),
-        slug: z.string().editor({ hidden: true }),
-        icon: z.string().optional().editor({ input: 'icon' }),
-        authors: z.array(z.object({
-          slug: z.string(),
-          username: z.string(),
-          name: z.string(),
-          to: z.string(),
-          avatar: z.object({
-            src: z.string(),
-            alt: z.string(),
-          }),
-        })),
-      }),
 			*/
+			schema: z.object({
+				draft: z.boolean().default(false),
+				category: z.enum(['Alps', 'Himalaya', 'Pyrenees']).optional(),
+				date: z.date(),
+				image: z.object({
+					src: z.string().editor({ input: 'media' }),
+					alt: z.string(),
+				}),
+				slug: z.string().editor({ hidden: true }),
+				icon: z.string().optional().editor({ input: 'icon' }),
+				authors: z.array(z.object({
+					slug: z.string(),
+					username: z.string(),
+					name: z.string(),
+					to: z.string(),
+					avatar: z.object({
+						src: z.string(),
+						alt: z.string(),
+					}),
+				})),
+			}),
 		}),
 	}
 })
