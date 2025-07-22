bb2103915bfc6344616c45db4569afe3460880d2
tvband <30714642+tvband@users.noreply.github.com>
2025-07-22 11:52:03 +0900

    一旦スキーマ削除

--- content.config.ts
+++ content.config.ts
@@ -5,37 +5,7 @@ export default defineContentConfig({
 		type1: defineCollection({
 			type: 'page',
 			source: '1.type1/*.md',
-			// メタデータの追加
-			/*
-			schema: z.object({
-				tags: z.array(z.string()),
-				image: z.string(),// stringでメディアライブラリ参照可能
-				date: z.date(),
-				draft: z.boolean(),
-				number: z.number(),
-			})
-			*/
-			schema: z.object({
-				draft: z.boolean().default(false),
-				category: z.enum(['Alps', 'Himalaya', 'Pyrenees']).optional(),
-				date: z.date(),
-				image: z.object({
-					src: z.string().editor({ input: 'media' }),
-					alt: z.string(),
-				}),
-				slug: z.string().editor({ hidden: true }),
-				icon: z.string().optional().editor({ input: 'icon' }),
-				authors: z.array(z.object({
-					slug: z.string(),
-					username: z.string(),
-					name: z.string(),
-					to: z.string(),
-					avatar: z.object({
-						src: z.string(),
-						alt: z.string(),
-					}),
-				})),
-			}),
+			
 		}),
 	}
 })
